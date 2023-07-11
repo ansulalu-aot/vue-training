122fe74cb5babe6f6c8cd109ae6d3e3ef1e20238
ansulalu-aot <ansulalu@aot-technologies.com>
2023-07-11 16:00:18 +0530

    Project-1

---
+++ project-1/src/router.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
+
+// lazy loading or dynamic import
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue'); 
+const CoachRegistation = () => import('./pages/coaches/CoachRegistration.vue'); 
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue'); 
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue'); 
+const UserAuth = () => import('./pages/auth/UserAuth.vue'); 
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      props: true,
+      children: [
+        { path: 'contact', component: ContactCoach }
+      ]
+    },
+    { path: '/register', component: CoachRegistation, meta: { requiresAuth: true } },
+    { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+    { path: '/:notFound(.*)', component: NotFound }
+  ]
+});
+
+router.beforeEach(function(to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
+});
+
+export default router;
